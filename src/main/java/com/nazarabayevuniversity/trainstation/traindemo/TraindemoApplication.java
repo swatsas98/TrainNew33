75bd7e47d83409f0a608354afe22861dafa33920
swatsas98 <swat.sas.98@gmail.com>
2019-10-21 21:10:53 +0600

    OOOO testtest

---
+++ src/main/java/com/nazarabayevuniversity/trainstation/traindemo/TraindemoApplication.java
@@ -0,0 +1,13 @@
+package com.nazarabayevuniversity.trainstation.traindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraindemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraindemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nazarabayevuniversity/trainstation/traindemo/TraindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazarabayevuniversity.trainstation.traindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraindemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
